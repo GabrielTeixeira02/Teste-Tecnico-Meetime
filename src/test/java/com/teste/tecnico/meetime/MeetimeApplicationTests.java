83c95346d12ac730e09a4fa12f3eade71e8340fa
Gabriel Teixeira <gabriel.teixeira@institutojef.org.br>
2025-03-25 13:41:38 -0300

    Configs

---
+++ src/test/java/com/teste/tecnico/meetime/MeetimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.tecnico.meetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
